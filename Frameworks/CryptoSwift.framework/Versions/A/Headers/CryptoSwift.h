acd3da73247dac47eef977f8c53b02a7b99e26f1
livedeal <zzz@gokuai.cn>
2018-11-07 10:24:57 +0800

    no message

---
+++ Frameworks/CryptoSwift.framework/Versions/A/Headers/CryptoSwift.h
@@ -0,0 +1,19 @@
+//
+//  CryptoSwift.h
+//  CryptoSwift
+//
+//  Created by Brandon on 2018/2/1.
+//  Copyright © 2018年 goukuai. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for CryptoSwift.
+FOUNDATION_EXPORT double CryptoSwiftVersionNumber;
+
+//! Project version string for CryptoSwift.
+FOUNDATION_EXPORT const unsigned char CryptoSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoSwift/PublicHeader.h>
+
+
